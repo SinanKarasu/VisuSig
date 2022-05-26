cc76db44b1c7089b85c8f504ba9c49a532de5b1c
Sinan Karasu <sinan@bozuk.org>
2022-05-26 09:46:45 -0700

    separation of code, data, contracts

--- VisuSig/VisualMesh/Model/EdgeBase.swift
+++ VisuSig/VisualMesh/Model/EdgeBase.swift
@@ -2,7 +2,7 @@
 import Foundation
 import CoreGraphics
 
-class EdgeBase: Identifiable, ObservableObject, Equatable, Hashable {
+class EdgeBase: Identifiable, ObservableObject {
     var id = UUID()
     @Published var startPort: PortBase
     @Published var endPort: PortBase
@@ -15,12 +15,6 @@ class EdgeBase: Identifiable, ObservableObject, Equatable, Hashable {
         self.data = CubicBezierData(edge: self)
     }
 
-//    init(data: CubicBezierData? ) {
-//        self.startPort = data!.from
-//        self.endPort = data!.to
-//        self.data = data
-//    }
-
     enum CodingKeys: CodingKey {
         case id
         case startPort
@@ -28,18 +22,6 @@ class EdgeBase: Identifiable, ObservableObject, Equatable, Hashable {
         case data
     }
 
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-//        hasher.combine(position.x)
-//        hasher.combine(position.y)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(startPort, forKey: .startPort)
-        try container.encode(endPort, forKey: .endPort)
-    }
 
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -65,10 +47,23 @@ struct EdgeProxy: Identifiable, Hashable {
     }
 }
 
-extension EdgeBase {
+extension EdgeBase : Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+}
+
+extension EdgeBase: Equatable {
     static func == (lhs: EdgeBase, rhs: EdgeBase) -> Bool {
         return lhs.startPort == rhs.startPort && lhs.endPort == rhs.endPort
     }
 }
 
-extension EdgeBase:Codable {}
+extension EdgeBase:Codable {
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(startPort, forKey: .startPort)
+        try container.encode(endPort, forKey: .endPort)
+    }
+}
