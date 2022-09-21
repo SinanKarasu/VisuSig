db8571d50ca0887ccda08ace50e695ece599b742
Sinan Karasu <sinan@bozuk.org>
2022-09-21 08:35:48 -0700

    refactor

---
+++ VisuSig/Additions/Extension+Color.swift
@@ -0,0 +1,17 @@
+//
+//  Extension+Color.swift
+//  VisuSig
+//
+//  Created by Sinan Karasu on 9/21/22.
+//
+
+import SwiftUI
+
+// built-in colors
+// black, brown, clear, cyan, gray, green, indigo, mint, orange, pink, purple, red, teal, white, yellow
+
+extension Color {
+    static let skyBlue = Color(red: 0.4627, green: 0.8392, blue: 1.0)
+    static let lemonYellow = Color(hue: 0.1639, saturation: 1, brightness: 1)
+    static let steelGray = Color(white: 0.4745)
+}
--- VisuSig/Host/MasterMenuView.swift
+++ VisuSig/Host/MasterMenuView.swift
@@ -14,12 +14,6 @@ struct MyBorder: View {
             .stroke(color, lineWidth: 5)
     }
 }
-// colors
-// black, brown, clear, cyan, gray, green, indigo, mint, orange, pink, purple, red, teal, white, yellow
-
-let skyBlue = Color(red: 0.4627, green: 0.8392, blue: 1.0)
-let lemonYellow = Color(hue: 0.1639, saturation: 1, brightness: 1)
-let steelGray = Color(white: 0.4745)
 
 struct MasterMenuView: View {
     
